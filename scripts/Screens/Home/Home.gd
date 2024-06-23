7e989b82775d7a7ff9af4692ac7290e36990e244
JonaPlaz <jplazanet@hotmail.com>
2024-06-23 15:24:26 +0200

    [Home] add brain aspiration texture

--- scripts/Screens/Home/Home.gd
+++ scripts/Screens/Home/Home.gd
@@ -1,1 +1,15 @@
 extends Node
+
+@onready var mainMenu = $MainMenu
+@onready var playMenu = $PlayMenu
+
+func _ready():
+	playMenu.hide()
+	
+func handleClickPlay():
+	playMenu.show()
+	mainMenu.hide()
+
+func handleClickPlayReturn():
+	playMenu.hide()
+	mainMenu.show()
